e3aaeb3796285c9219c87bbc3813415cbf0deaf0
Fausto Fusse <faustofusse@gmail.com>
2025-04-11 03:48:12 -0300

    feat: hola

---
+++ src/app/Components/topBar.tsx
@@ -0,0 +1,9 @@
+export function TopBar() {
+  
+  return (
+    <div className="flex flex-row justify-between max-w-full bg-grey">
+      <button className="bg-sky-500/10">Home</button>
+	    <button className="bg-sky-500/10">Sign in</button>
+    </div>
+  );
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,26 @@
 import { db } from "@/db";
 import { users as usersTable } from "@/db/schema";
+import { UserTab } from "./Components/userTab";
+import { TopBar } from "./Components/topBar";
 
 export default async function Home() {
-  const users = await db.select().from(usersTable);
-  
+  const users = await db.select().from(usersTable);;   
   return (
+    <>
+      <TopBar />
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      { users.map(user =>
-        <div key={user.id} className="flex flex-col gap-2">
-          <span>Nombre: {user.firstName}</span>
-          <span>Apellido: {user.lastName}</span>
-          <span>Email: {user.email}</span>
-          <span>ID: {user.id}</span>
-        </div>
+     { users.map(user =>
+        <>
+          <UserTab 
+            key={ user.id }
+            id={ user.id } 
+            firstName={ user.firstName ?? '' } 
+            lastName={ user.lastName ?? ''} 
+            email={ user.email ?? ''} 
+          />
+        </>
       )}
     </div>
+    </>
   );
 }
