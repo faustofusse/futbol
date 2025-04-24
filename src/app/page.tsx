39005b24abb7fbc158261e31b6de764caa08ec62
Fausto Fusse <faustofusse@gmail.com>
2025-04-23 21:49:07 -0300

    feat: navigation

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,25 +1,5 @@
-import { NavBar } from "@/components/navbar";
-import { db, groups as groupsTable, groupUsers } from "@/db";
-import { getSession } from "@/lib/sessions";
-import { eq, getTableColumns } from "drizzle-orm";
-import { redirect } from "next/navigation";
-
 export default async function Home() {
-  const session = await getSession();
-  if (!session) redirect('/login');
-
-  const groups = await db
-      .select({ ...getTableColumns(groupsTable) })
-      .from(groupUsers)
-      .where(eq(groupUsers.user, session.userId))
-      .leftJoin(groupsTable, eq(groupUsers.group, groupsTable.id));
-
-  return (
-    <>
-      <NavBar groups={groups} currentGroup={session.groupId} />
-      <span className="flex flex-1 justify-center">
-          userid: { session?.userId }
-      </span>
-    </>
-  );
+    return (
+        <span>plantilla</span>
+    );
 }
--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,15 +1,25 @@
 'use client';
 
 import { logout } from "@/lib/auth";
+import Link from "next/link";
 
 type Group = { id: number | null, name: string | null };
 
 export function NavBar({ groups, currentGroup }: { groups: Group[], currentGroup?: number }) {
-  const group = currentGroup == null ? groups.at(0) : groups.filter(g => g.id == currentGroup).at(0);
-  return (
-    <div className="p-4 flex flex-row justify-between max-w-full bg-grey">
-      <button className="bg-sky-500/10">{ group?.name }</button>
-	  <button onClick={logout} className="bg-sky-500/10">logout</button>
-    </div>
-  );
+    const group = currentGroup == null ? groups.at(0) : groups.filter(g => g.id == currentGroup).at(0);
+    return (
+        <div className="p-4 flex flex-row justify-between max-w-full bg-grey">
+            <div className="w-[20vh] flex flex-row justify-start">
+                <button className="bg-sky-500/10">{ group?.name }</button>
+            </div>
+            <div className="flex flex-row gap-4">
+                <Link href={"/"}>vista previa</Link>
+                <Link href={"/players"}>jugadores</Link>
+                <Link href={"/matches"}>partidos</Link>
+            </div>
+            <div className="w-[20vh] flex flex-row justify-end">
+                <button onClick={logout} className="bg-sky-500/10">logout</button>
+            </div>
+        </div>
+    );
 }
