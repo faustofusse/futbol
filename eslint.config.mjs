93a8029686d7921e92ba1f4e7c1b55fbd8d2725a
Fausto Fusse <faustofusse@gmail.com>
2025-05-01 19:50:38 -0300

    feat: prettier

---
+++ eslint.config.mjs
@@ -0,0 +1,33 @@
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+import { FlatCompat } from "@eslint/eslintrc";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const compat = new FlatCompat({
+  baseDirectory: __dirname,
+});
+
+const eslintConfig = [
+  ...compat.config({
+    extends: ["next/core-web-vitals", "next/typescript", "prettier"],
+    rules: {
+      semi: ["error"],
+      quotes: ["error", "double"],
+      "prettier/prettier": [
+        "error",
+        {
+          // singleQuote: false,
+          // trailingComma: "all",
+          // arrowParens: "avoid",
+          // endOfLine: "auto",
+          // printWidth: 80,
+          tabWidth: 2,
+        },
+      ],
+    }
+  })
+];
+
+export default eslintConfig;
